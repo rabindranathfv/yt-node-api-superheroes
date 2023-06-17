4c3ea0ee8b9ecc2c3990f71b2ab358ba48634e01
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-06-18 00:02:06 +0200

    agregando configuracion de loggers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import express from "express";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import hpp from "hpp";
 import helmet from "helmet";
 import cors from "cors";
 import displayRoutes from "express-routemap";
+import morgan from "morgan";
 
-import { API_VERSION, PORT, DB_HOST, DB_PORT, DB_NAME } from "./config/config.js";
+import {
+  API_VERSION,
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  LOG_FORMAT,
+} from "./config/config.js";
 import corsConfig from "./config/cors.config.js";
+import { logger, stream } from "./utils/logger.js";
 
 import superHeroesRoutes from "./routes/superheroes.routes.js";
 
@@ -17,19 +26,23 @@ const API_PREFIX = "api";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(hpp());
 app.use(helmet());
 app.use(cors(corsConfig));
+app.use(morgan(LOG_FORMAT ?? "../logs", { stream }));
 
-const MONGO_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+const MONGO_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-const connection = mongoose.connect(MONGO_URL).then((conn) => {
-  console.log(`DB Connection successfully ${MONGO_URL}`)
-}).catch((err) => {
-  console.log('🚀 ~ file: app.js:31 ~ connection ~ err:', err);
-})
+const connection = mongoose
+  .connect(MONGO_URL)
+  .then((conn) => {
+    logger.info(`DB Connection successfully ${MONGO_URL.slice(0, 10)}`);
+  })
+  .catch((err) => {
+    logger.error("🚀 ~ file: app.js:31 ~ connection ~ err:", err);
+  });
 
 app.use(`/${API_PREFIX}/${API_VERSION}/alive`, (req, res) => {
   res.json({
--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,14 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const { PORT, NODE_ENV, API_VERSION, ORIGIN, DB_HOST, DB_PORT, DB_NAME } = process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  API_VERSION,
+  ORIGIN,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  LOG_FORMAT,
+  LOG_DIR,
+} = process.env;
